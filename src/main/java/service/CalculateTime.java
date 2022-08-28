efb6eaece51be7f0b24f740dc2fb0b275ea1aeba
hukumran-san <71653875+hukumran-san@users.noreply.github.com>
2022-08-28 23:29:39 +0300

    v 0.0.3
    Ramadan end sacrifice holiday dates was calculated. all dates was controlled with this list. Religious holidays info was add into to object.

--- src/main/java/service/CalculateTime.java
+++ src/main/java/service/CalculateTime.java
@@ -6,6 +6,10 @@ import repository.DateRepositoryImpl;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.chrono.HijrahChronology;
+import java.time.chrono.HijrahDate;
+import java.time.temporal.ChronoField;
 import java.util.*;
 
 import static java.util.Calendar.*;
@@ -13,20 +17,24 @@ import static java.util.Calendar.*;
 public class CalculateTime {
     static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
     static Calendar cal = Calendar.getInstance();
-    static String firstDate = "19010101";
-    static String lastDate = "21000102";
+    static String firstDate = "19000101";
+    static String lastDate = "21000101";
     static List<DateTime> resultList = new ArrayList<>();
     static List<String> holidayMonthDayList = new ArrayList<>();
+    static List<String> hijrahHolidayList = new ArrayList<>();
     static DateRepository repository = new DateRepositoryImpl();
 
     static int workingDayOfYear = 0;
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException
+    {
+        getHijrahHolidays();
+
         createTurkishHolidayList();
 
         createFirstDate();
 
-        while (isDateReachToLastDate())
+        while (isDateReachToLastDate(cal))
         {
             createResultList();
         }
@@ -70,7 +78,7 @@ public class CalculateTime {
         dateTime.setWeekend((isWeekend(cal) ? 1 : 0));
         dateTime.setWorkingDayOfYear(getWorkingDayOfYear(cal));
         dateTime.setWeekEnding(dateFormat.format(getWeekEnding(cal)));
-        System.out.println(dateTime.toString());
+        System.out.println(dateTime);
     }
 
     private static int getWorkingDayOfYear(Calendar cal)
@@ -86,9 +94,9 @@ public class CalculateTime {
 
     private static Date getQuarterEndDate(Calendar cal) {
         Calendar localCalendar = Calendar.getInstance();
-        localCalendar.set(cal.get(cal.YEAR),cal.get(cal.MONTH),cal.get(cal.DATE));
+        localCalendar.set(cal.get(YEAR),cal.get(MONTH),cal.get(DATE));
         int quarterMonth = repository.getQuarterMonth(localCalendar.get(MONTH)/3);
-        localCalendar.set(cal.get(cal.YEAR),quarterMonth + 2,1);
+        localCalendar.set(cal.get(YEAR),quarterMonth + 2,1);
         localCalendar.set(localCalendar.get(YEAR),localCalendar.get(MONTH),localCalendar.getActualMaximum(DAY_OF_MONTH));
         return localCalendar.getTime();
     }
@@ -103,7 +111,7 @@ public class CalculateTime {
     private static Date getPreviousWorkingDate(Calendar cal)
     {
         Calendar localCalendar = Calendar.getInstance();
-        localCalendar.set(cal.get(cal.YEAR),cal.get(cal.MONTH),cal.get(cal.DATE));
+        localCalendar.set(cal.get(YEAR),cal.get(MONTH),cal.get(DATE));
 
         localCalendar.add(Calendar.DATE, -1);
 
@@ -114,7 +122,7 @@ public class CalculateTime {
         return localCalendar.getTime();
     }
 
-    private static boolean isDateReachToLastDate() {
+    private static boolean isDateReachToLastDate(Calendar cal) {
         return (Integer.parseInt(dateFormat.format(cal.getTime())) < Integer.parseInt(lastDate));
     }
 
@@ -123,9 +131,19 @@ public class CalculateTime {
                 cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY);
     }
 
-    private static boolean isDayHoliday(Calendar cal) {
-        return isWeekend(cal) ||
-                holidayMonthDayList.contains(dateFormat.format(cal.getTime()).substring(4, 8));
+    private static boolean isDayHoliday(Calendar cal)
+    {
+        return isWeekend(cal) || isPublicHoliday(cal) || isHijrahHoliday(cal);
+    }
+
+    private static boolean isHijrahHoliday(Calendar cal)
+    {
+        return hijrahHolidayList.contains(dateFormat.format(cal.getTime()));
+    }
+
+    private static boolean isPublicHoliday(Calendar cal)
+    {
+        return holidayMonthDayList.contains(dateFormat.format(cal.getTime()).substring(4, 8));
     }
 
     private static void addDateToCalendar() {
@@ -138,14 +156,14 @@ public class CalculateTime {
 
     static Date getMonthFirstDay(Calendar cal) {
         Calendar localCalendar = Calendar.getInstance();
-        localCalendar.set(cal.get(cal.YEAR),cal.get(cal.MONTH),cal.get(cal.DATE));
+        localCalendar.set(cal.get(YEAR),cal.get(MONTH),cal.get(DATE));
         localCalendar.set(Calendar.DAY_OF_MONTH, 1);
         return localCalendar.getTime();
     }
 
     static Date getMonthLastDay(Calendar cal) {
         Calendar localCalendar = Calendar.getInstance();
-        localCalendar.set(cal.get(cal.YEAR),cal.get(cal.MONTH),cal.get(cal.DATE));
+        localCalendar.set(cal.get(YEAR),cal.get(MONTH),cal.get(DATE));
         localCalendar.set(DAY_OF_MONTH, cal.getActualMaximum(DAY_OF_MONTH));
         return localCalendar.getTime();
     }
@@ -153,7 +171,7 @@ public class CalculateTime {
     static Date getNextWorkingDate(Calendar cal)
     {
         Calendar localCalendar = Calendar.getInstance();
-        localCalendar.set(cal.get(cal.YEAR),cal.get(cal.MONTH),cal.get(cal.DATE));
+        localCalendar.set(cal.get(YEAR),cal.get(MONTH),cal.get(DATE));
         localCalendar.add(Calendar.DATE, 1);
 
         while(isDayHoliday(localCalendar))
@@ -166,9 +184,9 @@ public class CalculateTime {
     static Date getQuarterStartDate(Calendar cal)
     {
         Calendar localCalendar = Calendar.getInstance();
-        localCalendar.set(cal.get(cal.YEAR),cal.get(cal.MONTH),cal.get(cal.DATE));
+        localCalendar.set(cal.get(YEAR),cal.get(MONTH),cal.get(DATE));
         int quarterMonth = repository.getQuarterMonth(localCalendar.get(MONTH)/3);
-        localCalendar.set(cal.get(cal.YEAR),quarterMonth,cal.getActualMinimum(DAY_OF_MONTH));
+        localCalendar.set(cal.get(YEAR),quarterMonth,cal.getActualMinimum(DAY_OF_MONTH));
         return localCalendar.getTime();
     }
 
@@ -180,5 +198,22 @@ public class CalculateTime {
         holidayMonthDayList.add("0715");
         holidayMonthDayList.add("0830");
         holidayMonthDayList.add("1029");
+    }
+
+    static List<String> getHijrahHolidays() throws ParseException
+    {
+        Calendar cal=Calendar.getInstance();
+        cal.setTime(dateFormat.parse(firstDate));
+        while (isDateReachToLastDate(cal))
+        {
+            HijrahDate islamicDate = HijrahChronology.INSTANCE.date(LocalDate.of(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH)+1, cal.get(Calendar.DATE)));
+            if(islamicDate.getLong(ChronoField.MONTH_OF_YEAR) == 10 && Arrays.asList(1,2,3).contains(((Long)islamicDate.getLong(ChronoField.DAY_OF_MONTH)).intValue()))//Calculate Ramadan Holidays
+                hijrahHolidayList.add(dateFormat.format(cal.getTime()));
+            else if(islamicDate.getLong(ChronoField.MONTH_OF_YEAR) == 12 && Arrays.asList(10,11,12,13).contains(((Long)islamicDate.getLong(ChronoField.DAY_OF_MONTH)).intValue()))//Calculate Sacrifice Holidays
+                hijrahHolidayList.add(dateFormat.format(cal.getTime()));
+            cal.add(DATE,1);
+        }
+        return hijrahHolidayList;
+
     }
 }
