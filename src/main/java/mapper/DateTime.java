dbf6497e2abe9ad1903d138c437ba8a84b468edd
hukumran-san <71653875+hukumran-san@users.noreply.github.com>
2022-08-28 22:26:17 +0300

    v 0.0.2
    dummy methods removed
    CalculateTime.java updated

--- src/main/java/mapper/DateTime.java
+++ src/main/java/mapper/DateTime.java
@@ -1,7 +1,5 @@
 package mapper;
 
-import java.util.Date;
-
 public class DateTime
 {
     private String calendarDate;
@@ -9,8 +7,8 @@ public class DateTime
     private String calendarMonthEndDate;
     private String nextWorkingDate;
     private String previousWorkingDate;
-    private String quarterEndDate;
     private String quarterStartDate;
+    private String quarterEndDate;
     private int dayOfWeek;
     private int dayOfYear;
     private int holiday;
@@ -22,184 +20,97 @@ public class DateTime
     private int year;
     private String charDate;
     private String day;
-    private String Month;
-    private String weekend;
-    private String weekEnding;
-    private String workingDayOfYear;
 
-    public String getCalendarDate() {
-        return calendarDate;
-    }
+    private String month;
+    private int weekend;
+    private String weekEnding;
+    private int workingDayOfYear;
 
     public void setCalendarDate(String calendarDate) {
         this.calendarDate = calendarDate;
-    }
-
-    public String getCalendarMonthBeginDate() {
-        return calendarMonthBeginDate;
     }
 
     public void setCalendarMonthBeginDate(String calendarMonthBeginDate) {
         this.calendarMonthBeginDate = calendarMonthBeginDate;
     }
 
-    public String getCalendarMonthEndDate() {
-        return calendarMonthEndDate;
-    }
-
     public void setCalendarMonthEndDate(String calendarMonthEndDate) {
         this.calendarMonthEndDate = calendarMonthEndDate;
-    }
-
-    public String getNextWorkingDate() {
-        return nextWorkingDate;
     }
 
     public void setNextWorkingDate(String nextWorkingDate) {
         this.nextWorkingDate = nextWorkingDate;
     }
 
-    public String getPreviousWorkingDate() {
-        return previousWorkingDate;
-    }
-
     public void setPreviousWorkingDate(String previousWorkingDate) {
         this.previousWorkingDate = previousWorkingDate;
-    }
-
-    public String getQuarterEndDate() {
-        return quarterEndDate;
     }
 
     public void setQuarterEndDate(String quarterEndDate) {
         this.quarterEndDate = quarterEndDate;
     }
 
-    public String getQuarterStartDate() {
-        return quarterStartDate;
-    }
-
     public void setQuarterStartDate(String quarterStartDate) {
         this.quarterStartDate = quarterStartDate;
-    }
-
-    public int getDayOfWeek() {
-        return dayOfWeek;
     }
 
     public void setDayOfWeek(int dayOfWeek) {
         this.dayOfWeek = dayOfWeek;
     }
 
-    public int getDayOfYear() {
-        return dayOfYear;
-    }
-
     public void setDayOfYear(int dayOfYear) {
         this.dayOfYear = dayOfYear;
-    }
-
-    public int getHoliday() {
-        return holiday;
     }
 
     public void setHoliday(int holiday) {
         this.holiday = holiday;
     }
 
-    public int getMonthDay() {
-        return monthDay;
-    }
-
     public void setMonthDay(int monthDay) {
         this.monthDay = monthDay;
-    }
-
-    public int getMonthNumber() {
-        return monthNumber;
     }
 
     public void setMonthNumber(int monthNumber) {
         this.monthNumber = monthNumber;
     }
 
-    public int getQuarter() {
-        return quarter;
-    }
-
     public void setQuarter(int quarter) {
         this.quarter = quarter;
-    }
-
-    public int getWeekNumber() {
-        return weekNumber;
     }
 
     public void setWeekNumber(int weekNumber) {
         this.weekNumber = weekNumber;
     }
 
-    public int getWeekOfMonth() {
-        return weekOfMonth;
-    }
-
     public void setWeekOfMonth(int weekOfMonth) {
         this.weekOfMonth = weekOfMonth;
-    }
-
-    public int getYear() {
-        return year;
     }
 
     public void setYear(int year) {
         this.year = year;
     }
 
-    public String getCharDate() {
-        return charDate;
-    }
-
     public void setCharDate(String charDate) {
         this.charDate = charDate;
-    }
-
-    public String getDay() {
-        return day;
     }
 
     public void setDay(String day) {
         this.day = day;
     }
 
-    public String getMonth() {
-        return Month;
-    }
-
     public void setMonth(String month) {
-        Month = month;
+        this.month = month;
     }
 
-    public String getWeekend() {
-        return weekend;
-    }
-
-    public void setWeekend(String weekend) {
+    public void setWeekend(int weekend) {
         this.weekend = weekend;
-    }
-
-    public String getWeekEnding() {
-        return weekEnding;
     }
 
     public void setWeekEnding(String weekEnding) {
         this.weekEnding = weekEnding;
     }
 
-    public String getWorkingDayOfYear() {
-        return workingDayOfYear;
-    }
-
-    public void setWorkingDayOfYear(String workingDayOfYear) {
+    public void setWorkingDayOfYear(int workingDayOfYear) {
         this.workingDayOfYear = workingDayOfYear;
     }
 
@@ -224,7 +135,7 @@ public class DateTime
                 ", year=" + year +
                 ", charDate='" + charDate + '\'' +
                 ", day='" + day + '\'' +
-                ", Month='" + Month + '\'' +
+                ", Month='" + month + '\'' +
                 ", weekend='" + weekend + '\'' +
                 ", weekEnding='" + weekEnding + '\'' +
                 ", workingDayOfYear='" + workingDayOfYear + '\'' +
--- src/main/java/service/CalculateTime.java
+++ src/main/java/service/CalculateTime.java
@@ -13,8 +13,8 @@ import static java.util.Calendar.*;
 public class CalculateTime {
     static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
     static Calendar cal = Calendar.getInstance();
-    static String firstDate = "19000101";
-    static String lastDate = "19000201";
+    static String firstDate = "19010101";
+    static String lastDate = "21000102";
     static List<DateTime> resultList = new ArrayList<>();
     static List<String> holidayMonthDayList = new ArrayList<>();
     static DateRepository repository = new DateRepositoryImpl();
@@ -47,29 +47,57 @@ public class CalculateTime {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date date = cal.getTime();
         Calendar localCalendar = cal;
+
         dateTime.setCalendarDate(sdf.format(date));
         dateTime.setCalendarMonthBeginDate(sdf.format(getMonthFirstDay(localCalendar)));
         dateTime.setCalendarMonthEndDate(sdf.format(getMonthLastDay(localCalendar)));
         dateTime.setNextWorkingDate(sdf.format(getNextWorkingDate(localCalendar)));
         dateTime.setPreviousWorkingDate(sdf.format(getPreviousWorkingDate(localCalendar)));
         dateTime.setQuarterStartDate(sdf.format(getQuarterStartDate(localCalendar)));
-        dateTime.setQuarterEndDate(sdf.format(getQuarterStartDate(localCalendar)));
+        dateTime.setQuarterEndDate(sdf.format(getQuarterEndDate(localCalendar)));
         dateTime.setCharDate(dateFormat.format(date));
         dateTime.setDay(repository.getDay(cal.get(Calendar.DAY_OF_WEEK)));
-        System.out.println(cal.get(Calendar.DAY_OF_WEEK));
-        dateTime.setDayOfWeek(cal.get(DAY_OF_WEEK_IN_MONTH));
+        dateTime.setDayOfWeek(cal.get(DAY_OF_WEEK));
         dateTime.setDayOfYear(cal.get(DAY_OF_YEAR));
         dateTime.setMonthDay(cal.get(DAY_OF_MONTH));
         dateTime.setHoliday(isDayHoliday(cal) ? 1 : 0);
         dateTime.setMonthNumber(cal.get(MONTH)+1);
         dateTime.setMonth(repository.getMonth(cal.get(MONTH)));
-        dateTime.setQuarter(repository.getQuarterMonth(localCalendar.get(MONTH))+1);
+        dateTime.setQuarter((localCalendar.get(MONTH)/3)+1);
         dateTime.setWeekNumber(cal.get(WEEK_OF_YEAR));
         dateTime.setWeekOfMonth(cal.get(WEEK_OF_MONTH));
         dateTime.setYear(cal.get(YEAR));
-        dateTime.setWeekend(String.valueOf(isWeekend(cal)));
-        dateTime.setWorkingDayOfYear(String.valueOf(isDayHoliday(cal) ? null : ++workingDayOfYear));
-        //System.out.println(dateTime.toString());
+        dateTime.setWeekend((isWeekend(cal) ? 1 : 0));
+        dateTime.setWorkingDayOfYear(getWorkingDayOfYear(cal));
+        dateTime.setWeekEnding(dateFormat.format(getWeekEnding(cal)));
+        System.out.println(dateTime.toString());
+    }
+
+    private static int getWorkingDayOfYear(Calendar cal)
+    {
+        if (cal.get(DAY_OF_YEAR)==1)
+            workingDayOfYear=0;
+
+        if(!isDayHoliday(cal))
+            return ++workingDayOfYear;
+        else
+            return 0;
+    }
+
+    private static Date getQuarterEndDate(Calendar cal) {
+        Calendar localCalendar = Calendar.getInstance();
+        localCalendar.set(cal.get(cal.YEAR),cal.get(cal.MONTH),cal.get(cal.DATE));
+        int quarterMonth = repository.getQuarterMonth(localCalendar.get(MONTH)/3);
+        localCalendar.set(cal.get(cal.YEAR),quarterMonth + 2,1);
+        localCalendar.set(localCalendar.get(YEAR),localCalendar.get(MONTH),localCalendar.getActualMaximum(DAY_OF_MONTH));
+        return localCalendar.getTime();
+    }
+
+    private static Date getWeekEnding(Calendar cal)
+    {
+        Calendar localCalendar = Calendar.getInstance();
+        localCalendar.set(cal.get(YEAR),cal.get(MONTH),cal.get(DATE)+(7-cal.get(DAY_OF_WEEK)));
+        return localCalendar.getTime();
     }
 
     private static Date getPreviousWorkingDate(Calendar cal)
@@ -139,7 +167,7 @@ public class CalculateTime {
     {
         Calendar localCalendar = Calendar.getInstance();
         localCalendar.set(cal.get(cal.YEAR),cal.get(cal.MONTH),cal.get(cal.DATE));
-        int quarterMonth = repository.getQuarterMonth(localCalendar.get(MONTH));
+        int quarterMonth = repository.getQuarterMonth(localCalendar.get(MONTH)/3);
         localCalendar.set(cal.get(cal.YEAR),quarterMonth,cal.getActualMinimum(DAY_OF_MONTH));
         return localCalendar.getTime();
     }
